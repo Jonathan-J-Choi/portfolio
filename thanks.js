5b584d695761e59fe0c3ed1b3f84f55aa8da060d
MrBachChoi <55605733+MrBachChoi@users.noreply.github.com>
2021-01-04 09:29:38 -0500

    added dark mode functionality to thank you page.

---
+++ thanks.js
@@ -0,0 +1,36 @@
+const checkbox = document.getElementById("checkbox");
+const nav = document.getElementById("nav");
+const section = document.getElementById("top");
+const footer = document.getElementById("footer");
+let timesClicked = 0;
+
+$("#checkbox").click(function() {
+  timesClicked++;
+  
+  if (timesClicked%2==0) {
+  //Revert back to Standard Mode
+  $(".tile.is-child.box, p.title").css({"background-color": "transparent", "color": "black"});
+  $(".ic").css({"color": "white"});
+  nav.classList.remove('is-black');
+  nav.classList.add('is-white');
+  document.body.classList.remove('has-background-dark');
+  document.body.classList.add('has-background-light');
+  section.classList.remove('is-warning');
+  section.classList.add('is-danger');
+  footer.classList.remove('footerDark');
+  footer.classList.add('footerLight');
+
+  } else {
+  //Dark Mode
+  $(".tile.is-child.box, p.title").css({"background-color": "#525252", "color": "#D3D3D3"});
+  $(".ic").css({"color": "black"});
+  nav.classList.remove('is-white');
+  nav.classList.add('is-black');
+  document.body.classList.remove('has-background-light');
+  document.body.classList.add('has-background-dark');
+  section.classList.remove('is-danger');
+  section.classList.add('is-warning');
+  footer.classList.remove('footerLight');
+  footer.classList.add('footerDark');
+  }
+  })

6478ebee1c690379ff623e0b29da2a96043f39aa
jonathan-j-choi <jonathan.junghoon.choi@gmail.com>
2021-11-03 16:28:43 -0400

    example WURFL.js code in script.js

--- script.js
+++ script.js
@@ -39,3 +39,13 @@ $("#checkbox").click(function() {
   footer.classList.add('footerDark');
   }
   })
+
+  /**
+ * Example of pushing WURFL capabilities to google analytics
+ */
+ga('send', 'pageview', {
+  'dimension1': WURFL.complete_device_name,
+  'dimension2': WURFL.form_factor,
+  'dimension3': WURFL.is_mobile,
+  'dimension4': WURFL.is_robot
+});
